005b3d7d8f7162f2d869a934500c9b44859a0bef
Bit0 <hosybaonhan2004@gmail.com>
2023-10-20 16:42:09 +0700

    updated state for cards

--- app/components/Box.js
+++ app/components/Box.js
@@ -1,13 +1,19 @@
 'use client'
+
+import { data } from "autoprefixer";
+
 export default function Box(props) {
     const { img, name, id } = props.data;
-
+    let isChoosen=props.isChoosen;
     return (
-      <div className="h-[35vh] bg-[#cfefff] mx-6 rounded-3xl cursor-pointer">
+      <div className="h-[35vh] bg-[#cfefff] mx-6 rounded-3xl cursor-pointer" onClick={props.click}>
         <img src={img} alt={name} className='h-[75%] w-full object-cover rounded-3xl'/>
-        <div className="h-[20%] w-[100%] px-4 mt-2">
+        <div className="h-[20%] w-[100%] mt-2 px-5">
           <h2 className="text-gray-800 font-semibold text-lg p-0">{name}</h2>
           <p className="text-gray-500 text-sm p-0">{window.location.hostname+'/'+id}</p>
+        </div>
+        <div className={isChoosen?"true":"false"+ " relative h-full w-full top-[-98.1%] left-0 rounded-3xl bg-[#2b2b2bb2] backdrop-blur-lg  false:none"}>
+
         </div>
       </div>
     );
--- app/manage/mypage/page.js
+++ app/manage/mypage/page.js
@@ -1,8 +1,51 @@
 'use client'
 import Box from "@/app/components/Box";
+import { useState } from "react";
 const data = [
   {
     id:1,
+    name:"Hồ Sỹ Bảo Nhân",
+    slogan:"Một thằng IT biết cài win và sửa mạng",
+    img:"https://www.w3schools.com/tags/img_girl.jpg",
+    contact: [
+      {
+        name:"Facebook",
+        url:"https://www.facebook.com/hosybaonhan"
+      },{
+        name:"Github",
+        url:"https://github.com/bobaonhan123"
+      }
+    ]
+  }, {
+    id:2,
+    name:"Hồ Sỹ Bảo Nhân",
+    slogan:"Một thằng IT biết cài win và sửa mạng",
+    img:"https://www.w3schools.com/tags/img_girl.jpg",
+    contact: [
+      {
+        name:"Facebook",
+        url:"https://www.facebook.com/hosybaonhan"
+      },{
+        name:"Github",
+        url:"https://github.com/bobaonhan123"
+      }
+    ]
+  }, {
+    id:3,
+    name:"Hồ Sỹ Bảo Nhân",
+    slogan:"Một thằng IT biết cài win và sửa mạng",
+    img:"https://www.w3schools.com/tags/img_girl.jpg",
+    contact: [
+      {
+        name:"Facebook",
+        url:"https://www.facebook.com/hosybaonhan"
+      },{
+        name:"Github",
+        url:"https://github.com/bobaonhan123"
+      }
+    ]
+  }, {
+    id:4,
     name:"Hồ Sỹ Bảo Nhân",
     slogan:"Một thằng IT biết cài win và sửa mạng",
     img:"https://www.w3schools.com/tags/img_girl.jpg",
@@ -19,16 +62,20 @@ const data = [
 ];
 
 export default function MyPage() {
-    
+    const [choosen,setChoosen] = useState(-1);
+    console.log(choosen)
     return (
-      <main className='pt-16 px-12'>
+      <main className='pt-16 px-12' onClick={()=>{setChoosen(-1)}}>
         <h1 className='text-3xl font-medium font-montserrat'>Danh thiếp của bạn</h1>
         <hr className='h-[1px] border-none bg-slate-900 rounded-sm relative bottom-[-1.3rem] z-0 mt-6'/>
         <button className='bg-[#cfefff] py-2 px-10 rounded-full z-10 relative mb-6'>Tạo mới</button>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-y-10">
           {data.map((item,index)=> {
             return (
-            <Box key={index} data={item}/>
+            <Box key={index} data={item} isChoosen={index!==choosen} click={(e)=>{
+              e.stopPropagation()
+              setChoosen(index);
+            }}/>
           )})}
         </div>
       </main>
