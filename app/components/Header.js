6f12f39117d5de619e4a535db91ce0b429d302bf
Bit0 <hosybaonhan2004@gmail.com>
2023-11-05 05:53:00 +0700

    updated layout edit page

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,17 +3,16 @@ import name from "@/app/img/name.png";
 import Image from "next/image";
 import Link from "next/link";
 export default function Header() {
-    return (
+  return (
     <>
-        <header className='flex items-center bg-[#cfefff] h-[9vh] fixed top-[2px] left-[2px] right-[2px] rounded-lg z-20'>
-            <Link href="/manage" className='h-full flex items-center ml-5'>
-            <div className='h-3/4 flex items-center'>
-                <Image src={logo} alt="logo" className='h-3/4 w-auto mx-1'/>
-                <Image src={name} alt="name" className='h-1/2 w-auto'/>
-            </div>
-            </Link>
-        </header>
+      <header className="flex items-center bg-[#cfefff] h-[9vh] fixed top-[2px] left-[2px] right-[2px] rounded-lg z-20">
+        <Link href="/manage" className="h-full flex items-center ml-5">
+          <div className="h-3/4 flex items-center">
+            <Image src={logo} alt="logo" className="h-3/4 w-auto mx-1" />
+            <Image src={name} alt="name" className="h-1/2 w-auto" />
+          </div>
+        </Link>
+      </header>
     </>
-    )
-  }
-  
+  );
+}
--- app/manage/mypage/edit/[profile]/page.js
+++ app/manage/mypage/edit/[profile]/page.js
@@ -1,13 +1,15 @@
-'use client'
-export default function Page({params}) {
+"use client";
+export default function Page({ params }) {
   return (
-  <div>
-    <div>
-    Post: {params.profile}
+    <div className="h-[90vh] flex">
+      <div className=" w-[50%] mx-[4%] my-[5%] h-full">
+        <div className=" h-[80%] w-[60%] bg-white border-2 border-black rounded-2xl">
+          Post: {params.profile}
+          </div>
+      </div>
+      <div className='w-[50%] m-4 h-full'>
+        <div></div>
+      </div>
     </div>
-    <div>
-      
-    </div>
-  </div>
-    )
+  );
 }
