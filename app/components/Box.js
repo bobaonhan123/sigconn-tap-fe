4ad691b5b96522f046b3eb48c0750465979d565d
Bit0 <hosybaonhan2004@gmail.com>
2023-11-05 10:07:12 +0700

    fix port

--- app/components/Box.js
+++ app/components/Box.js
@@ -6,12 +6,22 @@ import Link from "next/link";
 export default function Box(props) {
   const { img, name, id } = props.data;
   let isChoosen = props.isChoosen;
+  let hostname="";
+  let pathname="";
+  let port=""
+  if (typeof window !== 'undefined') {
+    hostname = window.location.hostname;
+    pathname = window.location.pathname;
+    port=window.location.port
+  }
+  console.log(hostname)
+  console.log(pathname)
   return (
     <div className="h-[35vh] bg-[#cfefff] mx-6 rounded-3xl cursor-pointer" onClick={props.click}>
       <img src={img} alt={name} className='h-[75%] w-full object-cover rounded-3xl' />
       <div className="h-[20%] w-[100%] mt-2 px-5">
         <h2 className="text-gray-800 font-semibold text-lg p-0">{name}</h2>
-        <p className="text-gray-500 text-sm p-0">{window.location.hostname + '/' + id}</p>
+        <p className="text-gray-500 text-sm p-0">{hostname +":"+port+'/' + id}</p>
       </div>
       { !isChoosen && <div className= {`relative 
         h-full 
@@ -27,7 +37,7 @@ export default function Box(props) {
         justify-evenly
         `}>
         <Link href={'/' + id } className={`text-white w-[55%] bg-[#003842] text-center rounded-full py-[3%]`}>Xem</Link>
-        <Link href={window.location.pathname + '/edit/'+id}
+        <Link href={pathname + '/edit/'+id}
          className={`text-white w-[55%] bg-[#737373] text-center rounded-full py-[3%]`}><div className="">Sửa</div></Link>
         <button
          className={`text-white w-[55%] bg-[#5271ff] text-center rounded-full py-[3%]`}>Xóa</button>
