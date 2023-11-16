3bdeae8f97ba8e2b50f84381083cc8b38c5e614c
Bit0 <hosybaonhan2004@gmail.com>
2023-11-17 06:55:16 +0700

    updated box

--- app/components/Box.js
+++ app/components/Box.js
@@ -6,15 +6,15 @@ import Link from "next/link";
 export default function Box(props) {
   const { img, name, id } = props.data;
   let isSelected = props.isSelected;
-  
+
   return (
     <div className="h-[35vh] bg-[#cfefff] mx-6 rounded-3xl cursor-pointer" onClick={props.click}>
       <img src={img} alt={name} className='h-[75%] w-full object-cover rounded-3xl' />
       <div className="h-[20%] w-[100%] mt-2 px-5">
         <h2 className="text-gray-800 font-semibold text-lg p-0">{name}</h2>
-        <p className="text-gray-500 text-sm p-0">{hostname+'/' + id}</p>
+        <p className="text-gray-500 text-sm p-0">{hostname + '/' + id}</p>
       </div>
-      { !isSelected && <div className= {`relative 
+      {!isSelected && <div className={`relative 
         h-full 
         w-full 
         top-[-98.1%] left-0 rounded-3xl bg-[#2b2b2b83] 
@@ -27,13 +27,14 @@ export default function Box(props) {
         items-center
         justify-evenly
         `}>
-        <Link href={'/' + id } className={`text-white w-[55%] bg-[#003842] text-center rounded-full py-[3%]`}>Xem</Link>
-        <Link href={'/manage/mypage/edit/'+id}
-         className={`text-white w-[55%] bg-[#737373] text-center rounded-full py-[3%]`}><div className="">Sửa</div></Link>
+        <a href={'/' + id} className='text-white w-[55%] bg-[#003842] text-center rounded-full py-[3%]'
+          target="_blank">Xem</a>
+        <Link href={'/manage/mypage/edit/' + id}
+          className={`text-white w-[55%] bg-[#737373] text-center rounded-full py-[3%]`}><div className="">Sửa</div></Link>
         <button
-         className={`text-white w-[55%] bg-[#5271ff] text-center rounded-full py-[3%]`}>Xóa</button>
+          className={`text-white w-[55%] bg-[#5271ff] text-center rounded-full py-[3%]`}>Xóa</button>
         <button
-         className={`text-white w-[55%] bg-[#ff5757] text-center rounded-full py-[3%]`}>Ghi thẻ</button>
+          className={`text-white w-[55%] bg-[#ff5757] text-center rounded-full py-[3%]`}>Ghi thẻ</button>
       </div>}
     </div>
   );
