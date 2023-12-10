d4a942ef7cd10446b75a9b653b8b01e5d7357a93
Bit0 <hosybaonhan2004@gmail.com>
2023-12-10 09:51:20 +0700

    update contact

--- app/components/editComponent/ContactEdit.js
+++ app/components/editComponent/ContactEdit.js
@@ -3,9 +3,34 @@ export default function ContactEdit({indexArr}) {
     return (
         <div className='h-[90%]
         w-full
-        bg-white
-        flex'>
-            {indexArr}
+        p-4
+        pl-8
+        '>
+            <h1 className='text-4xl
+            font-semibold
+            mb-16
+            '>Thông tin</h1>
+            <h3>Tiêu đề</h3>
+            <input type='text' className='
+            mb-6
+            h-12
+            pl-4
+            font-semibold
+            w-[80%]
+            rounded-xl
+            bg-[#cfefff]
+            '/>
+            <h3>Liên kết</h3>
+            <input type='text' className='
+            mb-16
+            h-12
+            pl-4
+            font-semibold
+            w-[80%]
+            rounded-xl
+            bg-[#cfefff]
+            underline
+            '/>
         </div>
     )
 }
